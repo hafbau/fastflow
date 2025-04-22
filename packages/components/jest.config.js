51952925b98184a92642b462acc3421b3b65b054
hafiz <hafbau@yahoo.com>
2025-04-22 22:20:14 +0400

    Task 3 done, all unit tests passing

--- packages/components/jest.config.js
+++ packages/components/jest.config.js
@@ -1,12 +1,19 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-    preset: 'ts-jest',
+export default {
+    preset: 'ts-jest/presets/js-with-ts-esm',
     testEnvironment: 'node',
     testMatch: ['**/__tests__/**/*.test.ts'],
-    moduleFileExtensions: ['ts', 'js'],
+    moduleFileExtensions: ['ts', 'js', 'json', 'node'],
     transform: {
-        '^.+\\.ts$': 'ts-jest',
+        '^.+\\.ts$': [
+            'ts-jest',
+            {
+                tsconfig: 'tsconfig.json',
+                useESM: true
+            }
+        ]
     },
+    extensionsToTreatAsEsm: ['.ts', '.tsx', '.mts', '.cts'],
     collectCoverage: true,
     coverageDirectory: 'coverage',
     coverageReporters: ['text', 'lcov'],
@@ -14,10 +21,5 @@ module.exports = {
         '/node_modules/',
         '/__tests__/',
         '/dist/'
-    ],
-    globals: {
-        'ts-jest': {
-            tsconfig: 'tsconfig.json'
-        }
-    }
+    ]
 } 
--- packages/server/jest.config.js
+++ packages/server/jest.config.js
@@ -1,6 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-    preset: 'ts-jest',
+export default {
+    preset: 'ts-jest/presets/js-with-ts-esm',
     testEnvironment: 'node',
     moduleNameMapper: {
         '^langchain/(.*)$': '<rootDir>/../../node_modules/langchain/dist/$1',
@@ -22,5 +22,6 @@ module.exports = {
     testPathIgnorePatterns: ['/node_modules/', '/dist/'],
     collectCoverage: true,
     coveragePathIgnorePatterns: ['/node_modules/', '/dist/'],
-    testMatch: ['**/__tests__/**/*.test.ts']
+    testMatch: ['**/__tests__/**/*.test.ts'],
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
 } 
